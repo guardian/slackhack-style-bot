51089786c3b299c28e6dd4479ec6e7da823fb4b8
Robert Rees <robert.rees@guardian.co.uk>
2015-07-22 11:41:51 +0100

    Adds some bold styling to the picture credit text

--- styleguide/p.py
+++ styleguide/p.py
@@ -1,14 +1,14 @@
 messages = {
 	"picture_credit":
-		"""
-		Please always sub the credits on photos in Composer.
+"""
+Please *always sub the credits* on photos in Composer.
 
-		Because of how some agencies credit their images, they sometimes pull in the same details to both the 'Photographer' and 'Source' fields (see 1st attachment). This means the caption is doubled up and looks really ugly and unprofessional (attachment 2).
+Because of how some agencies credit their images, they sometimes pull in the same details to both the 'Photographer' and 'Source' fields (see 1st attachment). This means the caption is doubled up and looks really ugly and unprofessional (attachment 2).
 
-		Style for agencies is: Reuters (not REUTERS), Rex (not REX).
+*Style for agencies* is: Reuters (not REUTERS), Rex (not REX).
 
-		Also, please always check the alt text on the trail picture. If it is unsubbed (attachment 3) please just click 'Copy main photo' and sub as you see fit. 
+Also, please always check the *alt text* on the trail picture. If it is unsubbed (attachment 3) please just click 'Copy main photo' and sub as you see fit. 
 
-		Remember, alt text should always just be a short caption explaining what is in the picture for people who use screen-reading software; it should not be a list of keywords, nor should it be a caption that relates to the story rather than the image eg it should say: David Cameron at PMQs, not David Cameron Commons PMQs, or David Cameron told the Commons he would not back down on his austerity measures.
-		""",
+Remember, alt text should always just be a short caption explaining what is in the picture for people who use screen-reading software; it should not be a list of keywords, nor should it be a caption that relates to the story rather than the image eg it should say: David Cameron at PMQs, not David Cameron Commons PMQs, or David Cameron told the Commons he would not back down on his austerity measures.
+""",
 }
