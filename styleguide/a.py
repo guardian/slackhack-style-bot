abacdb642657939def18f536f3cae9c42d77eab4
Robert Rees <robert.rees@guardian.co.uk>
2015-07-22 12:16:06 +0100

    Adds Chapter A

---
+++ styleguide/a.py
@@ -0,0 +1,12 @@
+messages = {
+	"about":
+"""
+This is a simple call and response bot.
+
+It's written in Python (natch) but I deployed it on AWS Elastic Beanstalk which was a first for me and quite interesting. It wasn't very complicated.
+
+What is cool about this hack?
+
+The big thing is actually if we had a database that represented the styleguide (here we are faking it with flat files). If we had this then we could generate the online documentation, automate Twitter queries and implement a chat bot from the same information.
+""",
+}
